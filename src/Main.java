7dbebad45b25226b9fec33ed41ed81a3f5d3bba7
origin <kruglov56832@mail.ru>
2024-06-09 15:26:16 +0900

    homeWork_13.1

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,34 @@
-import com.sun.jdi.connect.Connector;
-
 public class Main {
-    public static void main(String [] args) {
-        /*
-        Создайте класс Book, который будет содержать в себе данные о названии,
-        авторе и годе публикации книги. Убедитесь, что типы полей класса Book выбраны правильно:
-        это String, Author и int
-         */
-
+    public static void main(String[] args) {
         System.out.println("Задача №1");
-
-        /*
-        Создайте класс Author, который содержит в себе данные об имени и фамилии автора.
-         */
-
         System.out.println("Задача №2");
-
-        /*
-        Напишите конструкторы для обоих классов, заполняющие все поля.
-         */
-
         System.out.println("Задача №3");
-
-        //Создайте геттеры для всех полей автора и всех полей книги.
-
         System.out.println("Задача №4");
-
-        //Создайте сеттер для поля «Год публикации» у книги.
-
         System.out.println("Задача №5");
 
-        /*
-        В методе main создайте несколько объектов «Книга» (достаточно двух) и несколько объектов «Автор»
-        (достаточно тоже двух) и инициализируйте их. Учтите, что авторы являются обязательными и книги не могут
-        создаваться без авторов.Метод main не должен находиться в классах Book и Author.
-         */
-
         System.out.println("Задача №6");
+        Author author1 = new Author("Lev", "Tolstoy");
+        Author author2 = new Author("Fyodor", "Dostoevsky");
 
-        Author nameAuthor = new Author("Lev", "Tolstoy");
-        System.out.println(nameAuthor.getName());
-        System.out.println(nameAuthor.getSurname());
+        Book book1 = new Book("War and Peace", author1, 1869);
+        Book book2 = new Book("Crime and Punishment", author2, 1866);
 
-        Book publicBook = new Book(nameAuthor, 1988);
-        System.out.println(publicBook.getPublicationYear());
-        System.out.println(nameAuthor.getName() + " " + nameAuthor.getSurname());
-
-        //В том же методе main измените год публикации одной из книг с помощью сеттера.
+        System.out.println(author1.getName());
+        System.out.println(author1.getSurname());
+        System.out.println(book1.getTitle());
+        System.out.println(book1.getAuthor().getName() + " " + book1.getAuthor().getSurname());
+        System.out.println(book1.getPublicationYear());
 
         System.out.println("Задача №7");
+        book1.setPublicationYear(2024);
+        System.out.println(book1.getPublicationYear());
 
-        publicBook.setPublicationYear(2024);
-        System.out.println(publicBook.getPublicationYear());
+        System.out.println("Задача №8");
+        System.out.println(author1.toString());
+        System.out.println(book1.toString());
+
+        System.out.println(book1.equals(book2));
+        System.out.println(book1.hashCode());
+        System.out.println(book2.hashCode());
     }
 }
